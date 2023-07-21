94b23bf7155e138c4eed7441a48c5eec751b2d22
jhekau <mail@jheka.ru>
2023-07-21 16:34:41 +0300

    decorative changes

--- internal/files/export_test.go
+++ internal/files/export_test.go
@@ -11,6 +11,7 @@ import (
 	types_ "github.com/jhekau/favicon/internal/core/types"
 )
 
+// for /convert.go
 func ConvertFileTest( 
 	source, source_svg, save types_.FilePath,
 	typ types_.FileType,
@@ -23,13 +24,15 @@ func ConvertFileTest(
 	fn_source_check = func( fpath types_.FilePath, source_typ types_.FileType, thumb_size int ) error {
 		return nil
 	}
-
 	return convert_file(source, source_svg, save, typ, size_px, conv)
 }
 
 var ConvertICOTest = convert_ico
 var ConvertPNGTest = convert_png
 
+
+
+// for /check.go 
 func SourceResolutionTest( fpath types_.FilePath, FnOsOpen func(name string) (*os.File, error) ) ( w,h int, err error ) {
 	osOpen = FnOsOpen
 	return source_resolution( fpath )
