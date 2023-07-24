f92d6e8a37432421b6a387551e174a018a647762
jhekau <mail@jheka.ru>
2023-07-24 14:09:36 +0300

    append internal config

---
+++ internal/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+/* *
+ * Copyright (c) 2023, @jhekau <mr.evgeny.u@gmail.com>
+ * 24 July 2023
+ * files test
+ */
+import ()
+
+const (
+	ImageSizeMin = 16
+	ImageSizeMax = 40_000
+)
+
