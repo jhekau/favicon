f7124e0b3e980f4edf52b6ff4e3fb92c2a96d18b
jhekau <mail@jheka.ru>
2023-07-26 10:32:50 +0300

    hide log function

--- internal/service/convert/checks/preview.go
+++ internal/service/convert/checks/preview.go
@@ -6,20 +6,21 @@ package checks
  * проверка исходного изображения
  */
 import (
-	err_ "github.com/jhekau/favicon/internal/core/err"
+	// err_ "github.com/jhekau/favicon/internal/core/err"
 	types_ "github.com/jhekau/favicon/internal/core/types"
 	config_ "github.com/jhekau/favicon/internal/config"
 )
-
+/*
 const (
-	// logP01 = `P01: `
-	// logP02 = `P02: `
-	// logP03 = `P03: `
-	// logP04 = `P04: `
+	logP01 = `P01: `
+	logP02 = `P02: `
+	logP03 = `P03: `
+	logP04 = `P04: `
 )
 func errP(i... interface{}) error {
 	return err_.Err(err_.TypeError, `/internal/service/convert/checks/preview.go`, i...)
-} 
+}
+*/
 
 type Preview struct {}
 
