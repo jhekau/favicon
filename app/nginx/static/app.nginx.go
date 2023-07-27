50d890f4c980733a0841c1d035892b3e2f4bfa00
jhekau <mail@jheka.ru>
2023-07-27 16:10:28 +0300

    updade, and append file

--- app/nginx/static/app.nginx.go
+++ app/nginx/static/app.nginx.go
@@ -1,8 +1,9 @@
-package appnginx
+package app_nginx_static
 
 /* *
  * Copyright (c) 2023, @jhekau <mr.evgeny.u@gmail.com>
  * 21 July 2023
+ * статическое создание конфига без управления самим nginx
  */
 import ()
 
---
+++ internal/service/nginx/static/nginx.go
@@ -0,0 +1,8 @@
+package nginx_static
+
+/* *
+ * Copyright (c) 2023, @jhekau <mr.evgeny.u@gmail.com>
+ * 26 July 2023
+ * создание конфига для nginx на основе имеющегося набора изображений
+ */
+ import ()
