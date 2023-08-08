7d4caff4b224cb6b2b63fe1266e1b660593b1ad2
jhekau <mail@jheka.ru>
2023-08-08 17:29:11 +0300

    recreate mock

--- internal/mocks/intr/service/convert/convert.go
+++ internal/mocks/intr/service/convert/convert.go
@@ -5,11 +5,81 @@
 package mock_convert
 
 import (
+	io "io"
 	reflect "reflect"
 
 	types "github.com/jhekau/favicon/internal/core/types"
+	convert "github.com/jhekau/favicon/internal/service/convert"
+	domain "github.com/jhekau/favicon/pkg/domain"
 	gomock "go.uber.org/mock/gomock"
 )
+
+// MockStorageOBJ is a mock of StorageOBJ interface.
+type MockStorageOBJ struct {
+	ctrl     *gomock.Controller
+	recorder *MockStorageOBJMockRecorder
+}
+
+// MockStorageOBJMockRecorder is the mock recorder for MockStorageOBJ.
+type MockStorageOBJMockRecorder struct {
+	mock *MockStorageOBJ
+}
+
+// NewMockStorageOBJ creates a new mock instance.
+func NewMockStorageOBJ(ctrl *gomock.Controller) *MockStorageOBJ {
+	mock := &MockStorageOBJ{ctrl: ctrl}
+	mock.recorder = &MockStorageOBJMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use.
+func (m *MockStorageOBJ) EXPECT() *MockStorageOBJMockRecorder {
+	return m.recorder
+}
+
+// IsExists mocks base method.
+func (m *MockStorageOBJ) IsExists() (bool, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "IsExists")
+	ret0, _ := ret[0].(bool)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// IsExists indicates an expected call of IsExists.
+func (mr *MockStorageOBJMockRecorder) IsExists() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IsExists", reflect.TypeOf((*MockStorageOBJ)(nil).IsExists))
+}
+
+// Key mocks base method.
+func (m *MockStorageOBJ) Key() domain.StorageKey {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Key")
+	ret0, _ := ret[0].(domain.StorageKey)
+	return ret0
+}
+
+// Key indicates an expected call of Key.
+func (mr *MockStorageOBJMockRecorder) Key() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Key", reflect.TypeOf((*MockStorageOBJ)(nil).Key))
+}
+
+// Read mocks base method.
+func (m *MockStorageOBJ) Read() (io.ReadCloser, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Read")
+	ret0, _ := ret[0].(io.ReadCloser)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Read indicates an expected call of Read.
+func (mr *MockStorageOBJMockRecorder) Read() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Read", reflect.TypeOf((*MockStorageOBJ)(nil).Read))
+}
 
 // MockConverterT is a mock of ConverterT interface.
 type MockConverterT struct {
@@ -35,7 +105,7 @@ func (m *MockConverterT) EXPECT() *MockConverterTMockRecorder {
 }
 
 // Do mocks base method.
-func (m *MockConverterT) Do(source, save types.FilePath, size_px int, typ types.FileType) (bool, error) {
+func (m *MockConverterT) Do(source, save convert.StorageOBJ, size_px int, typ types.FileType) (bool, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Do", source, save, size_px, typ)
 	ret0, _ := ret[0].(bool)
@@ -110,15 +180,15 @@ func (m *MockCheckSource) EXPECT() *MockCheckSourceMockRecorder {
 }
 
 // Check mocks base method.
-func (m *MockCheckSource) Check(fpath types.FilePath, originalSVG bool, thumb_size int) error {
+func (m *MockCheckSource) Check(original convert.StorageOBJ, originalSVG bool, thumb_size int) error {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Check", fpath, originalSVG, thumb_size)
+	ret := m.ctrl.Call(m, "Check", original, originalSVG, thumb_size)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
 // Check indicates an expected call of Check.
-func (mr *MockCheckSourceMockRecorder) Check(fpath, originalSVG, thumb_size interface{}) *gomock.Call {
+func (mr *MockCheckSourceMockRecorder) Check(original, originalSVG, thumb_size interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Check", reflect.TypeOf((*MockCheckSource)(nil).Check), fpath, originalSVG, thumb_size)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Check", reflect.TypeOf((*MockCheckSource)(nil).Check), original, originalSVG, thumb_size)
 }
