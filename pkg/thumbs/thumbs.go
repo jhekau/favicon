010e94f9dad8a9d5c529b731df480aa964c227da
jhekau <mail@jheka.ru>
2023-08-13 17:28:43 +0300

    add func NewThumb

--- pkg/thumbs/thumbs.go
+++ pkg/thumbs/thumbs.go
@@ -6,9 +6,6 @@ package thumbs
  */
 import (
 	"io"
-	"log"
-	"net/http"
-	"net/url"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -26,7 +23,6 @@ import (
 	thumb_ "github.com/jhekau/favicon/internal/service/thumb"
 	files_ "github.com/jhekau/favicon/internal/storage/files"
 
-	types_ "github.com/jhekau/favicon/pkg/core/types"
 	converter_ "github.com/jhekau/favicon/pkg/models/converter"
 	logger_ "github.com/jhekau/favicon/pkg/models/logger"
 	storage_ "github.com/jhekau/favicon/pkg/models/storage"
@@ -40,29 +36,16 @@ const (
 	logT04 = `T04: get file thumb`
 	logT05 = `T05: get file manifest`
 	logT06 = `T06: error read original`
+	logT07 = `T07: get new thumb`
 )
 
 var (
-	TypePNG = types_.PNG()
-	TypeICO = types_.ICO()
-	// TypeSVG = types_.SVG()
+	TypePNG = thumb_.PNG
+	TypeICO = thumb_.ICO
 
-	// ~~ interface ~~
-
-	DefaultList = default_list
-
-	/* -------------------------------------------------------------------------------------------
-	 * CustomList(
-	 *	ThumbNEW().SetHREF(`touch-icon-iphone.png`).SetSize(180).SetTypeImage(favicon.TypePNG),
-	 *	ThumbNEW().SetHREF(`/icon-192.png`).SetSize(192).SetTypeImage(favicon.TypePNG),
-	 *	ThumbNEW().SetHREF(`/icon-512.png`).SetSize(512).SetTypeImage(favicon.TypePNG),
-	 * )
-	 * ------------------------------------------------------------------------------------------- */
-	CustomList = custom_list
-
-	ThumbNEW = func() *thumb_.Thumb {
-		return &thumb_.Thumb{}
-	}
+	// ThumbNEW = func() *thumb_.Thumb {
+	// 	return thumb_.NewThumb(key string, typThumb thumb_.Typ, l logger_.Logger, s storage_.Storage, c converter_.Converter)
+	// }
 )
 
 // создание пустого набора превьюх для одного оригинального изображения
@@ -136,13 +119,23 @@ func (t *Thumbs) ConvertSet( conv converter_.Converter ) {
 	t.mu.Unlock()
 }
 
-
-func (t *Thumbs) Append(thumb *thumb_.Thumb) *Thumbs {
-	return t.append(thumb)
+func (t *Thumbs) NewThumb(key string, typThumb thumb_.Typ) (*thumb_.Thumb, error) {
+	tb, err := thumb_.NewThumb(key, typThumb, t.l, t.storage, t.conv)
+	if err != nil {
+		return nil, t.l.Error(logTP, logT07, err)
+	}
+	return tb, nil
 }
 
-func (t *Thumbs) Handle() {
-	t.handle()
+func (t *Thumbs) AppendThumb(tb *thumb_.Thumb) *Thumbs {
+	return t.append(tb)
+}
+
+
+
+// получение конкретной превьюхи для отправки пользователю
+func (t *Thumbs) File(urlPath string) (content io.ReadSeekCloser, modtime time.Time, name string, exists bool, err error) {
+	return t.thumbFile(urlPath)
 }
 
 
@@ -159,53 +152,6 @@ func (t *Thumbs) append(thumb *thumb_.Thumb) *Thumbs {
 	t.thumbs[thumb.URLPathGet()] = thumb
 	return t
 }
-/*
-func (t *Thumbs) set_folder_work(folder string) *Thumbs {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-
-	t.folder_work = typ_.Folder(folder)
-	return t
-}
-func (t *Thumbs) get_folder_work() typ_.Folder {
-	t.mu.RLock()
-	defer t.mu.RUnlock()
-
-	return t.folder_work
-}
-*/
-
-func (t *Thumbs) handle() {
-	http.Handle(`/`, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-		default:
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-
-		content, modtime, name, exists, err := t.thumbFile(r.URL.Path)
-		if err != nil {
-			log.Println(t.l.Error(logT01, err))
-			w.WriteHeader(http.StatusInternalServerError)
-		} else if !exists {
-			w.WriteHeader(http.StatusNotFound)
-		} else {
-			http.ServeContent(w, r, name, modtime, content)
-		}
-	}))
-}
-
-// func (t *Thumbs) contentFile(url_ *url.URL) (content io.ReadSeekCloser, modtime time.Time, name string, exists bool, err error) {
-
-// 	if c,tm,n,e,er := t.thumbFile(url_); err != nil {
-// 		err = t.l.Error(logTP, logT03, er)
-// 		return
-// 	} else if exists {
-// 		return c,tm,n,e,err
-// 	}
-// 	return ``, false, nil
-// }
 
 func (t *Thumbs) thumbFile(urlPath string) (content io.ReadSeekCloser, modtime time.Time, name string, exists bool, err error) {
 
@@ -226,22 +172,6 @@ func (t *Thumbs) thumbFile(urlPath string) (content io.ReadSeekCloser, modtime t
 	return
 }
 
-// func (t *Thumbs) server_file_manifest(url_ *url.URL) (manifest typ_.FilePath, exists bool, err error) {
-
-// 	if !t.manifest.URLExists(url_) {
-// 		return ``, false, nil
-// 	}
-
-// 	manifest, exists, err = t.manifest.GetFile(t.get_folder_work(), t.thumbs)
-// 	if err != nil {
-// 		return ``, false, t.l.Error(logTP, logT05, err)
-// 	}
-// 	return manifest, exists, nil
-// }
-
-// func (T *Thumbs) manifest_url_exists( URLpath string ) ( fpath string, exists bool, err error )
-// func (t *Thumbs) get_thumbs() map[types_.URLHref/*clear*/]*thumb_.Thumb
-
 func (t *Thumbs) tags_html() string {
 
 	var tags strings.Builder
@@ -259,29 +189,4 @@ func (t *Thumbs) tags_html() string {
 func (t *Thumbs) getThumb(u typ_.URLPath) (*thumb_.Thumb, bool) {
 	tb, ok := t.thumbs[u]
 	return tb, ok
-}
-
-func default_list() *Thumbs {
-	return &Thumbs{
-		thumbs: func() map[typ_.URLPath]*thumb_.Thumb {
-			m := map[typ_.URLPath]*thumb_.Thumb{}
-			for _, thumb := range defaults_.Defaults() {
-				m[thumb.URLPathGet()] = thumb
-			}
-			return m
-		}(),
-		manifest: manifest_.Default(),
-	}
-}
-
-func custom_list(thumbs ...*thumb_.Thumb) *Thumbs {
-	return &Thumbs{
-		thumbs: func() map[typ_.URLPath]*thumb_.Thumb {
-			m := map[typ_.URLPath]*thumb_.Thumb{}
-			for _, thumb := range thumbs {
-				m[thumb.URLPathGet()] = thumb
-			}
-			return m
-		}(),
-	}
 }
