e874ae075d9dcdf450de38661dfc8fa4c9020459
jhekau <mail@jheka.ru>
2023-08-14 10:07:25 +0300

    add handler

--- internal/adapters/http/v1/handler.go
+++ internal/adapters/http/v1/handler.go
@@ -5,10 +5,30 @@ package adapter_http_v1
  * 27 July 2023
  */
 import (
+	"net/http"
 
+	logger_default_ "github.com/jhekau/favicon/internal/core/logs/default"
+	handler_ "github.com/jhekau/favicon/pkg/core/http/v1/handler"
+	thumbs_ "github.com/jhekau/favicon/pkg/service/thumbs"
 )
 
-
 func Run() {
+
+	// gracefull shotdown
+
+	logger := &logger_default_.Logger{}
 	
+	icons, err := thumbs_.NewThumbs_DefaultsIcons()
+	if err != nil {
+		// error
+	}
+	icons.LoggerSet(logger)
+	// manifest
+
+	handle := (&handler_.Handler{}).Handle(icons)
+	err = http.ListenAndServe(`\`, handle)
+	if err != nil {
+		// error
+	}
+
 }
