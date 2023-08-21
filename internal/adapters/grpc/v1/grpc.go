b7da52430c8722694a8bd1e491917602cc1748aa
jhekau <mail@jheka.ru>
2023-08-21 12:08:55 +0300

    edit

--- internal/adapters/grpc/v1/grpc.go
+++
@@ -1,8 +0,0 @@
-package adapter_grpc_v1
-
-/* *
- * Copyright (c) 2023, @jhekau <mr.evgeny.u@gmail.com>
- * 21 August 2023
- */
-import()
-
--- pkg/transport/grpc/v1/grpc.go
+++
@@ -1,8 +0,0 @@
-package transport_grpc
-
-/* *
- * Copyright (c) 2023, @jhekau <mr.evgeny.u@gmail.com>
- * 21 August 2023
- */
-import()
-
